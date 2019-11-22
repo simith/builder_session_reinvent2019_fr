6bb5b10a140f90e9f65b9d5961008aba01fa40f1
Ubuntu <ubuntu@ip-172-31-12-94.ap-southeast-1.compute.internal>
2019-11-22 09:57:20 +0000

    changes to create keys script

--- workshop/tools/create_keys_and_certificate.sh
+++ workshop/tools/create_keys_and_certificate.sh
@@ -1,8 +1,7 @@
 json_output=$(aws iot create-keys-and-certificate)
-echo $json_output
 
 certificate=$(echo ${json_output} | jq  .certificatePem | sed 's/\\n/\n/g'| tr -d '"'  > cert.pem)
 privatekey=$(echo ${json_output} | jq  .keyPair.PrivateKey | sed 's/\\n/\n/g'| tr -d '"'  > privatekey.pem)
 certificateId=$(echo ${json_output} | jq  .certificateId | sed 's/\\n/\n/g'| tr -d '"'  > certificateId)
-echo $certificate
+cat certificateId
 echo "Not printing Private key, stored in privatekey.pem"
