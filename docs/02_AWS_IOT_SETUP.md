58d98358fde07ab701790e346c1aaa58d5403cc2
simithn@gmail.com <simithn@186590d21625.ant.amazon.com>
2019-11-23 09:46:41 +0800

    updates to README

--- docs/02_AWS_IOT_SETUP.md
+++ docs/02_AWS_IOT_SETUP.md
@@ -2,29 +2,23 @@
 
 In order to connect succesfully to AWS IoT, a X.509 certificate, Private key and an IoT Policy attached to the Certificate is required. We will create this first.
 
-Go to *builder_session_reinvent2019_fr/workshop/tools/* directory of the git repository and execute **create_keys_and_certificate.sh** as below,
+Go to _builder_session_reinvent2019_fr/workshop/tools/_ directory of the git repository and execute **create_keys_and_certificate.sh** as below,
 
 ```
 builder_session_reinvent2019_fr/workshop/tools$./create_keys_and_certificate.sh
-Certificate Id: 161202e390728e42144cdc9x9b35b60b358c5dedc2f36f11108xxc24a5fc0233
+Certificate Id: 161202e390728e42144cdc969b35b60b358c5dedc2f36f1110820c24a5fc0233
 Not printing Private key, stored in privatekey.pem
 ```
 
 Note down the Certificate Id printed on the console.
 
-The script creates 3 files in the tools directory by making calls to AWS IoT Core in the us-west-2 region, 
+The script creates 3 files in the tools directory by making calls to AWS IoT Core in the us-west-2 region,
 
 1. cert.pem (Device certificate)
 2. privatekey.pem (Private Key)
 3. certificateId (Certificate Id)
 
-## 2. Activate the AWS IoT Device certificate
-Head to the AWS IoT Console (us-west-2), and from the sidebar, select Secure > Certificates, copy the Certificate Id from the output of the **create_keys_and_certificate.sh** and paste it into the searchbox,
+The script also activates the certificate, creates an IoT policy, attaches an IoT Policy to the Certificate, creates a Thing and associates the Thing with the Certificate.
 
-
-
-
-
-
-
-
+IoT Policy: {certificate-id}-policy
+Thing name: {certificate-id}-thing
--- workshop/tools/create_keys_and_certificate.sh
+++ workshop/tools/create_keys_and_certificate.sh
@@ -1,3 +1,9 @@
+
+
+workshop_tools_dir=$(pwd)
+workshop_esp32_partition_writer_dir=../../amazon-freertos/vendors/espressif-espressif-idf/components/ \
+nvs_flash/nvs_partition_generator/
+
 json_output=$(aws iot create-keys-and-certificate)
 
 $(echo ${json_output} | jq  .certificatePem | sed 's/\\n/\n/g'| tr -d '"'  > cert.pem)
@@ -5,4 +11,35 @@ $(echo ${json_output} | jq  .keyPair.PrivateKey | sed 's/\\n/\n/g'| tr -d '"'  >
 $(echo ${json_output} | jq  .certificateId | sed 's/\\n/\n/g'| tr -d '"'  > certificateId)
 certificateIdVar=$(cat certificateId)
 echo "Certificate Id: ${certificateIdVar}"
-echo "Not printing Private key, stored in privatekey.pem"
+echo "[OK]] Certitficate stored in cert.pem, privatekey in privatekey.pem"
+
+# creae a code signing certificate
+echo "Creating Code signing Key and Certificate"
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve -outform PEM -out ecdsasigner.key
+openssl req -new -x509 -config code_signing_cert_config.txt -extensions my_exts -nodes -days 365 -key ecdsasigner.key -out ecdsasigner.crt
+
+# Create PEM to DER conversion
+echo "Converting certificate,private key and code signing key from PEM to DER format"
+openssl rsa -inform PEM -outform der   <privatekey.key >privatekey.der
+openssl x509 -outform der -in cert.pem -out cert.der
+openssl x509 -outform der -in ecdsasigner.crt -out csk.der
+
+echo "Copying certificate,private key and code signing key to ${workshop_esp32_partition_writer_dir}"
+cp privatekey.der cert.der csk.der ${workshop_esp32_partition_writer_dir}
+cd ${workshop_esp32_partition_writer_dir}
+echo "Creating partition.bin with Key, Device certificate and Code signing certificate"
+python  nvs_partition_gen.py  --version v2 ./partition.csv partition.bin
+cp partition.bin ${workshop_tools_dir}
+echo "Updating certificate state to ACTIVE"
+$(aws iot update-certificate --certificate-id ${certificateIdVar} --new-status=ACTIVE)
+echo "Creating IoT Policy with name [${certificateIdVar}-iot-policy]"
+$(aws iot create-policy --policy-name ${certificateIdVar}-iot-policy --policy-document file://iot_policy.json)
+echo "Attaching IoT Policy [${certificateIdVar}-iot-policy] to certificate ${certificateIdVar}"
+$(aws iot attach-policy --polcy-name ${certificateIdVar}-iot-policy --target ${certificateIdVar})
+echo "Creating Thing with thing name ${certificateIdVar}-thing"
+$(aws iot create-thing ${certificateIdVar}-thing)
+echo "Attaching Thing with thing name ${certificateIdVar}-thing to to certificate ${certificateIdVar}"
+$(aws iot attach-thing-principal --thing-name ${certificateIdVar}-thing --principal ${certificateIdVar})
+cd ${workshop_tools_dir}
+echo "partition.bin copied to ${workshop_tools_dir}, ready for download"
+
