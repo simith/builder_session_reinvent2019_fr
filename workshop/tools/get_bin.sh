8d5e8a39c96a2f97b572dd22279d1609c2c3bc57
Ubuntu <ubuntu@ip-172-31-29-251.us-west-2.compute.internal>
2019-11-29 09:42:19 +0000

    adding get_bin.sh

---
+++ workshop/tools/get_bin.sh
@@ -0,0 +1,9 @@
+WORKSHOP_TOOLS_DIR=$WORKSHOP_ROOT_DIR/workshop/tools
+WORKSHOP_FREERTOS_DIR=$WORKSHOP_ROOT_DIR/workshop/amazon-freertos
+
+cp $WORKSHOP_FREERTOS_DIR/build/bootloader/bootloader.bin $WORKSHOP_TOOLS_DIR/bin/
+cp $WORKSHOP_FREERTOS_DIR/build/partition_table/partition-table.bin $WORKSHOP_TOOLS_DIR/bin/
+cp $WORKSHOP_FREERTOS_DIR/build/aws_demos.bin $WORKSHOP_TOOLS_DIR/bin/firmware.bin
+cp $WORKSHOP_FREERTOS_DIR/build/ota_data_initial.bin $WORKSHOP_TOOLS_DIR/bin/ota_data_inital.bin
+
+echo "Copied partition-table.bin,bootloader.bin,ota_data_initial.bin and firmware.bin to ./bin folder"
